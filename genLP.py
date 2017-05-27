aa4c17bace6d64308b1fced7e7d924edc566421c
George <gmd44@uclive.ac.nz>
2017-05-27 13:03:36 +1200

    saturday 2

--- genLP.py
+++ genLP.py
@@ -10,6 +10,26 @@ def print_aux(X,Y,Z):
                     
      print(aux,end="")
      
+     for k in range(1,Y+1):
+          mysum = ""
+          for i in range(1,X+1):
+               mysum += "+ u{}{} ".format(i,k)
+          for j in range(1,Z+1):
+               mysum += "- v{}{} ".format(k,j)
+               
+          mysum += "= 0"
+          print(mysum[2:])
+     
+     
+def print_objective_constraints(X,Y,Z):
+     for k in range(1,Y+1):
+          con = "l - "
+          for i in range(1,X+1):
+               con += "u{}{} - ".format(i,k)
+               
+          con = con[:-2] + "= 0"
+          
+          print(con)
      
 def print_demand(X,Y,Z):
      '''prints demand constraints'''
@@ -18,20 +38,21 @@ def print_demand(X,Y,Z):
           for j in range(1,Z+1):
                mysum1 = mysum2 = mysum3 = ""
                for k in range(1,Y+1):
-                    mysum1 += "w{}{}{} +".format(i,k,j)
-                    mysum2 += "w{0}{1}{2} x{0}{1}{2} +".format(i,k,j)
-                    mysum3 += "x{}{}{} +".format(i,k,j)
+                    demand += "x{0}{1}{2} - {3}w{0}{1}{2} = 0\n".format(i,k,j,(i+j)/3)
+                    mysum1 += "w{}{}{} + ".format(i,k,j)
+                    mysum2 += "w{0}{1}{2} x{0}{1}{2} + ".format(i,k,j)
+                    mysum3 += "x{}{}{} + ".format(i,k,j)
                     
                
-               mysum1 = mysum1[:-1] + "= "
-               mysum2 = mysum2[:-1] + "= "
-               mysum3 = mysum3[:-1] + "= "
+               mysum1 = mysum1[:-2] + "= "
+               mysum2 = mysum2[:-2] + "= "
+               mysum3 = mysum3[:-2] + "= "
                
                mysum1 += "3"
                mysum2 += str(i + j)
                mysum3 += str(i + j)
                
-               demand += mysum1 + "\n" + mysum2 + "\n" + mysum3+ "\n"
+               demand += mysum1 + "\n" #+ mysum2 + "\n" + mysum3+ "\n"
                          
                 
               
@@ -43,13 +64,13 @@ def print_capp(X,Y,Z):
      for i in range(1,X+1):
           for k in range(1,Y+1):
                capp += "u{0}{1} - c{0}{1} <= 0\n".format(i,k)
-               capp += "u{0}{1} - c{0}{1} r <= 0\n".format(i,k)
+               
                
                
      for k in range(1,Y+1):
           for j in range(1,Z+1):
                capp += "v{0}{1} - d{0}{1} <= 0\n".format(k,j)
-               capp += "v{0}{1} - d{0}{1} r <= 0\n".format(k,j)
+               
                
      print(capp,end="")
      
@@ -75,7 +96,7 @@ def print_nonneg(X,Y,Z):
           for j in range(1,Z+1):
                nonneg += "0 <= v{}{}\n".format(k,j)
                
-     nonneg += "0 <= r\n"
+     nonneg += "0 <= l\n"
      
      print(nonneg,end="")
 
@@ -88,9 +109,10 @@ def main():
      
      print("X = {}, Y = {}, Z = {}".format(X,Y,Z))
      print("Minimize")
-     print(" r")
+     print(" l")
      print("Subject to")
      print_aux(X,Y,Z)
+     print_objective_constraints(X,Y,Z)
      print_demand(X,Y,Z)
      print_capp(X,Y,Z)
      print("Bounds")
